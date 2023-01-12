cad624dd7ce84bf53b6eda5368b1c3763ff7a28d
Paul Meyer <paul.meyer@mind7.fr>
2023-01-12 14:07:52 +0100

    fixed connector id not displaying in editor

--- internals/models/connectorconfig.go
+++ internals/models/connectorconfig.go
@@ -3,6 +3,6 @@ package models
 type ConnectorConfig struct {
 	Id          int64       `json:"id"`
 	Name        string      `json:"name"`
-	ConnectorId string      `json:"connector_id"`
+	ConnectorId string      `json:"connectorId"`
 	Current     interface{} `json:"current"`
 }
