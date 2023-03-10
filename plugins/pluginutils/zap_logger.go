b1969cd3be337730d8808ce4d1af7b6786e98f34
hamzajismani <hamza.jismani@mind7.fr>
2023-03-10 10:06:58 +0100

    update files after review

--- plugins/pluginutils/zap_logger.go
+++ plugins/pluginutils/zap_logger.go
@@ -2,6 +2,7 @@ package pluginutils
 
 import (
 	// "fmt"
+	"fmt"
 	"io"
 	"log"
 
@@ -111,27 +112,27 @@ func (w ZapWrapper) SetLevel(lvl Level) {
 // Code below prevents a panic, if wrong arguments set received.
 func convertToZapAny(args ...interface{}) []zapcore.Field {
 	fields := []zapcore.Field{}
-	// for i := len(args); i > 0; i -= 2 {
-	// 	left := i - 2
-	// 	if left < 0 {
-	// 		left = 0
-	// 	}
+	for i := len(args); i > 0; i -= 2 {
+		left := i - 2
+		if left < 0 {
+			left = 0
+		}
 
-	// 	items := args[left:i]
+		items := args[left:i]
 
-	// 	switch l := len(items); l {
-	// 	case 2:
-	// 		k, ok := items[0].(string)
-	// 		if ok {
-	// 			fields = append(fields, zap.Any(k, items[1]))
-	// 		} else {
-	// 			fields = append(fields, zap.Any(fmt.Sprintf("arg%d", i-1), items[1]))
-	// 			fields = append(fields, zap.Any(fmt.Sprintf("arg%d", left), items[0]))
-	// 		}
-	// 	case 1:
-	// 		fields = append(fields, zap.Any(fmt.Sprintf("arg%d", left), items[0]))
-	// 	}
-	// }
+		switch l := len(items); l {
+		case 2:
+			k, ok := items[0].(string)
+			if ok {
+				fields = append(fields, zap.Any(k, items[1]))
+			} else {
+				fields = append(fields, zap.Any(fmt.Sprintf("arg%d", i-1), items[1]))
+				fields = append(fields, zap.Any(fmt.Sprintf("arg%d", left), items[0]))
+			}
+		case 1:
+			fields = append(fields, zap.Any(fmt.Sprintf("arg%d", left), items[0]))
+		}
+	}
 
 	return fields
 }
