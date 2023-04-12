330f80c596573be0488a9367f909a96137ce5750
cedricablain <ablain.cedric@gmail.com>
2023-04-12 19:34:35 +0200

    Minor syntax fixes

--- internals/email/service.go
+++ internals/email/service.go
@@ -5,26 +5,21 @@ import (
 )
 
 var (
-	_globalMu    sync.RWMutex
+	_globalMu     sync.RWMutex
 	_globalSender *Sender
 )
 
-// S is used to access the global Email Sender 
+// S is used to access the global Email Sender
 func S() *Sender {
 	_globalMu.RLock()
 	defer _globalMu.RUnlock()
-
 	c_sender := _globalSender
-	return  c_sender
+	return c_sender
 }
 
 // Init initialize the global Email Sender singleton
 func InitSender(username string, password string, host string, port string) {
 	_globalMu.Lock()
 	defer _globalMu.Unlock()
-	
-	
 	_globalSender = NewSender(username, password, host, port)
 }
-
-
--- internals/email/service_test.go
+++ internals/email/service_test.go
@@ -6,32 +6,21 @@ import (
 )
 
 // InitUnitTest initialize the global Email sender singleton in unite test mode
-func TestInitSender_and_S(t *testing.T) {
-
-    // Initializing the values ​​for the test 
+func TestInitSender(t *testing.T) {
 	host := os.Getenv("TEST_SMTP_HOST")
 	port := os.Getenv("TEST_SMTP_PORT")
 	username := os.Getenv("TEST_SMTP_USERNAME")
 	password := os.Getenv("TEST_SMTP_PASSWORD")
 
-    // Call INitSender function
-	InitSender(username,password,host,port)
+	InitSender(username, password, host, port)
 
-	// Sender Instance Recovery
 	sender := S()
-
-	// Checking if the Sender instance is non-zero
-	if sender == nil{
+	if sender == nil {
 		t.Error("Expected a non-nil Sender instancer, but got nil")
 	}
 
-	// Recovery of a second instance of the Sender
 	sender2 := S()
-
-	// Checking if the two instances are the same (sngleton )
 	if sender != sender2 {
-		t.Errorf("Expected the same Sender instance, but got different instances")
+		t.Error("Expected the same Sender instance, but got different instances")
 	}
-    
 }
-
