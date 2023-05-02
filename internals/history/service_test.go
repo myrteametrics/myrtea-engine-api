f7c0b26f02d917340d73279940d52883a5011e07
ALI OMAR Ismail <73675505+Ismail731404@users.noreply.github.com>
2023-05-02 15:17:42 +0200

    New scheduler jobs CompactHistoryJob and PurgeHistoryJob (#87)
    * new job purge combact
    
    * add fields fromOffset and ToOffset instead of FromTS and TOTs
    
    * Support proper compact and purge history method
    
    * change parse Duration function  and purge struct
    
    * resolved conversion review
    
    * Update internals/scheduler/compact_history_job.go
    
    * Update internals/scheduler/compact_history_job.go
    
    * Update internals/scheduler/purge_history_job.go
    
    * Update internals/scheduler/purge_history_job.go
    
    ---------
    
    Co-authored-by: ALI OMAR Ismail <liamsi2019#gmail.com>
    Co-authored-by: cedricablain <ablain.cedric@gmail.com>
    Co-authored-by: cedricablain <68556787+cedricablain@users.noreply.github.com>

--- internals/history/service_test.go
+++ internals/history/service_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/myrteametrics/myrtea-engine-api/v5/internals/tests"
 )
 
-func TestPurge(t *testing.T) {
+func TestCompact(t *testing.T) {
 	t.SkipNow()
 
 	db := tests.DBClient(t)
@@ -21,5 +21,21 @@ func TestPurge(t *testing.T) {
 	}
 	interval := "day"
 
-	New(db).PurgeHistory(options, interval)
+	New(db).CompactHistory(options, interval)
+}
+
+func TestPurge(t *testing.T) {
+	t.SkipNow()
+
+	db := tests.DBClient(t)
+
+	options := GetHistorySituationsOptions{
+		SituationID:         -1,
+		SituationInstanceID: -1,
+		FromTS:              time.Time{},
+		ToTS:                time.Now().Add(-1 * 24 * time.Hour),
+		ParameterFilters:    make(map[string]string),
+	}
+
+	New(db).PurgeHistory(options)
 }
