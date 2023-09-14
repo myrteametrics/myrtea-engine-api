94b26fb4b76bdc37701a4770797a91eb9e9e3ec0
Hamza NOURSAID <hamza.noursaid@gmail.com>
2023-09-14 14:13:30 +0200

    change sdk version

--- internals/router/logger_zap.go
+++ internals/router/logger_zap.go
@@ -51,7 +51,7 @@ func CustomZapRequestLogger(f chimiddleware.LogFormatter) func(next http.Handler
 
 				zapFields = append(zapFields,
 					zap.Duration("lat", time.Since(t1)),
-					zap.Int("status", ww.Status()),
+					zap.Int("http_status", ww.Status()),
 					zap.Int("size", ww.BytesWritten()),
 				)
 
