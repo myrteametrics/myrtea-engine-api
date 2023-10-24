62a5b023e9432b08db2525559d973c6a263762d0
ALI OMAR Ismail <liamsi2019@gmail.com>
2023-10-24 23:25:27 +0200

    Management of CRUD operations for parameters from technical settings.

--- internals/router/routes.go
+++ internals/router/routes.go
@@ -175,6 +175,13 @@ func engineRouter() http.Handler {
 
 	r.Get("/facts/{id}/export", handlers.ExportFact)
 
+	r.Get("/variablesconfig", handlers.GetVariablesConfig)
+	r.Get("/variablesconfig/{id}", handlers.GetVariableConfig)
+	r.Get("/variablesconfig/key/{key}", handlers.GetVariableConfigByKey)
+	r.Post("/variablesconfig", handlers.PostVariableConfig)
+	r.Put("/variablesconfig/{id}", handlers.PutVariableConfig)
+	r.Delete("/variablesconfig/{id}", handlers.DeleteVariableConfig)
+
 	return r
 }
 
