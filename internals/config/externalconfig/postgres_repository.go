f9dd19f253e49b4d9c34fcfda020a19f631ab990
ALI OMAR Ismail <liamsi2019@gmail.com>
2024-09-30 20:31:18 +0200

    Use Squirrel to align SQL queries with the rest of the project

--- internals/config/externalconfig/postgres_repository.go
+++ internals/config/externalconfig/postgres_repository.go
@@ -198,9 +198,11 @@ func (r *PostgresRepository) Update(id int64, externalConfig models.ExternalConf
 
 	// Count total versions
 	var versionCount int
-	err = r.conn.QueryRow(`
-		SELECT COUNT(*) FROM external_generic_config_versions_v1 WHERE config_id = $1
-	`, id).Scan(&versionCount)
+	err = r.newStatement().
+		Select("COUNT(*)").
+		From("external_generic_config_versions_v1").
+		Where("config_id = ?", id).
+		QueryRow().Scan(&versionCount)
 	if err != nil {
 		tx.Rollback() // Cancel transaction in case of error
 		return err
@@ -210,18 +212,17 @@ func (r *PostgresRepository) Update(id int64, externalConfig models.ExternalConf
 	maxVersions := viper.GetInt("MAX_EXTERNAL_CONFIG_VERSIONS_TO_KEEP")
 
 	if versionCount > maxVersions {
-		_, err = r.conn.Exec(`
-			DELETE FROM external_generic_config_versions_v1 
-			WHERE config_id = $1 
-			AND created_at < (
-				SELECT created_at 
-				FROM external_generic_config_versions_v1 
-				WHERE config_id = $1 
-				ORDER BY created_at DESC 
-				OFFSET $2
-				LIMIT 1
-			)
-		`, id, maxVersions)
+		_, err = r.newStatement().
+			Delete("external_generic_config_versions_v1").
+			Where("config_id = ?", id).
+			Where("created_at < ?", r.newStatement().
+				Select("created_at").
+				From("external_generic_config_versions_v1").
+				Where("config_id = ?", id).
+				OrderBy("created_at DESC").
+				Offset(uint64(maxVersions)).
+				Limit(1)).
+			Exec()
 		if err != nil {
 			tx.Rollback() // Cancel transaction in case of error
 			return err
