dd296ce32986996239575c13f9d0b5c4b9b4e4a8
watolua <mathieu.de-jesus-prestataire@laposte.fr>
2025-12-10 16:35:48 +0100

    Refine error message for invalid 'cc' parameter in situation reporting

--- internal/tasker/situation_reporting.go
+++ internal/tasker/situation_reporting.go
@@ -78,7 +78,7 @@ func buildSituationReportingTask(parameters map[string]interface{}) (SituationRe
 	} else if val == "" {
 		task.Cc = []string{}
 	} else {
-		return task, errors.New("missing or invalid 'cc' parameter (string not empty required)")
+		return task, errors.New("Invalid 'cc' parameter (string not empty required)")
 	}
 
 	if val, ok := parameters["attachmentFileNames"].(string); ok && val != "" {
