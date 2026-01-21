8e20398c021f68d73758a39ae08fbde8a89ec82d
WatoLua <32160224+WatoLua@users.noreply.github.com>
2026-01-21 11:14:15 +0100

    Make `HomePage` field a pointer to allow null values in `Role` struct. (#254)
    * Make `HomePage` field a pointer to allow null values in `Role` struct.
    
    * Update GitHub Actions configuration to use 'main' branch and upgrade checkout action to v6
    
    ---------
    
    Co-authored-by: SchawnnDev <iperskill@gmail.com>

--- pkg/security/roles/role.go
+++ pkg/security/roles/role.go
@@ -7,5 +7,5 @@ import (
 type Role struct {
 	ID       uuid.UUID `json:"id"`
 	Name     string    `json:"name"`
-	HomePage string    `json:"homePage,omitempty"`
+	HomePage *string   `json:"homePage,omitempty"`
 }
